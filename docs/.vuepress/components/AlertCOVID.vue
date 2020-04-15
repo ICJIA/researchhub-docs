66c00b42c303b4dd6d86838b2abbefeb76057f73
bobaekang <bkang.dev@gmail.com>
2020-04-15 12:42:03 -0500

    fix: use alert message from api v2

--- docs/.vuepress/components/AlertCOVID.vue
+++ docs/.vuepress/components/AlertCOVID.vue
@@ -1,21 +1,35 @@
 <template>
-  <p class="alert">
+  <div class="alert">
     <span class="icon" v-html="$options.static.mdiAddCircle"></span>
-    View up to date information on how Illinois is handling
-    the Coronavirus Disease 2019 (COVID-19) from the
-    <a
-      href="http://coronavirus.illinois.gov"
-      target="_blank"
-      rel="noreferrer"
-    >
-      State of Illinois Coronavirus Response Site
-      <span class="icon" v-html="$options.static.mdiLaunch"></span>
-    </a>
-  </p>
+
+    <span v-if="success" v-html="html"></span>
+    <span v-else>
+      Please reload the page to view the latest COVID-19 information.
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      html: "",
+      success: false
+    };
+  },
+  async created() {
+    try {
+      const res = await fetch("https://coronavirus.icjia-api.cloud/v2/");
+      const { html } = await res.json();
+      this.html = html
+        .replace(/<(div|i).*?>|<\/(div|i)>/g, "")
+        .replace(". ", '.<br style="padding: 0 1rem">')
+        .trim();
+      this.success = true;
+    } catch {
+      this.success = false;
+    }
+  },
   static: {
     mdiAddCircle:
       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="16px" width="16px" role="img" aria-hidden="true"><path fill="red" d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"></path></svg>',
@@ -29,13 +43,17 @@ export default {
 .alert {
   background-color: rgba(33, 150, 243, 0.12);
   color: #2196f3 !important;
+  display: flex;
+  align-items: center;
   font-family: "Lato";
+  line-height: 1.5;
   padding: 1rem;
 }
 
 .alert .icon {
   display: inline-flex;
   vertical-align: middle;
+  margin-right: 1rem;
 }
 
 .alert a {
